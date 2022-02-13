1079ed74fbe0c99efb40443d57c645b2e2ff2864
T1masv <timotei.gerante@gmail.com>
2022-02-13 16:58:14 +0100

    hotfix : build

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,28 +1,24 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { MatTabsModule } from '@angular/material/tabs';
-import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
-
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppComponent } from './app.component';
-import { GestionListComponent } from './gestion-list/gestion-list.component';
 import { HeaderComponent } from './header/header.component';
 import { RouterModule } from '@angular/router';
 import { FooterComponent } from './footer/footer.component';
-
+import { GestionListComponent } from './gestion-list/gestion-list.component';
+import { CardComponent } from './card/card.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     FooterComponent,
+    CardComponent,
     GestionListComponent
   ],
-  imports: [
-    BrowserModule,
-    RouterModule,
-    MatTabsModule
-  ],
+  imports: [BrowserModule, RouterModule, MatTabsModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
